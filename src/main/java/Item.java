4f7e94753613441731bdec63dc60de3fd1ccdd5c
Tee <123083698+Titewithane@users.noreply.github.com>
2023-10-07 20:20:53 +0700

    add constructor msg

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -33,12 +33,13 @@ class Room extends Item{
     private static double total_rooms;
     public Room(String type, double rate) {
         super(type, rate);
+        System.out.printf("%-22srate = %,10.2f     rate++ = %,10.2f\n", this.getType(), this.getRate(), this.getRatePlus());
     }
     public Room(String type, double rate, int qty) {super(type, rate, qty);}
 
     //rate with service charge & vat
     public double getRatePlus() {
-        return rate + ((rate + (rate * 0.1)) * 0.07);
+        return rate + (rate * 0.1) + ((rate + (rate * 0.1)) * 0.07);
     }
 
 }
@@ -47,6 +48,7 @@ class Meal extends Item {
     private static int total_meals;
     public Meal (String type, double rate) {
         super(type, rate);
+        System.out.printf("%-22srate = %,10.2f     rate++ = %,10.2f\n", this.getType(), this.getRate(), this.getRate());
     }
     public Meal (String type, double rate, int qty) {
         super(type, rate, qty);
@@ -59,31 +61,41 @@ class HotelList {
         String character, type;
         String[] lines = new String[3];
         String path = "src/main/java/";
+        String filename = file;
         ArrayList<Item> hotel = new ArrayList<Item>();
+        boolean openSuccess = false;
+        Scanner keyboardScan = new Scanner(System.in);
 
-        try (
-                Scanner scan = new Scanner(new FileReader(path + file));
-        ){
-            while(scan.hasNext()) {
-                String line = scan.nextLine();
-                lines = line.split(",");
-                character = lines[0].trim();
-                type = lines[1].trim();
-                rate = Double.parseDouble(lines[2].trim());
-
-                if(character.equals("R")) {
-                    Room room = new Room(type, rate);
-                    hotel.add(room);
-                } else if(character.equals("M")) {
-                    Meal meal = new Meal(type, rate);
-                    hotel.add(meal);
-                } else {
-                    //throw exception invalid character or something.
+        while (!openSuccess) {
+            try (
+                    Scanner scan = new Scanner(new FileReader(path + filename));
+            ) {
+                openSuccess = true;
+                System.out.printf("Read hotel data from file %s%s \n\n", path, filename);
+                while (scan.hasNext()) {
+                    String line = scan.nextLine();
+                    lines = line.split(",");
+                    character = lines[0].trim();
+                    type = lines[1].trim();
+                    rate = Double.parseDouble(lines[2].trim());
+                    if (character.equals("R")) {
+                            Room room = new Room(type, rate);
+                            hotel.add(room);
+                    } else if (character.equals("M")) {
+                            Meal meal = new Meal(type, rate);
+                            hotel.add(meal);
+                    } else {
+                            //throw exception invalid character or something.
+                    }
                 }
+            } catch (FileNotFoundException e) {
+                System.out.println(e);
+                System.out.println("Enter file name for hotel data = ");
+                filename = keyboardScan.nextLine();
             }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
         }
+
+        keyboardScan.close();
         return hotel;
     }
 }
