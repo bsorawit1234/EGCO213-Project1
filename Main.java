a60fe3de87ec0e98623ed39c20af19106aab3e61
bsorawit1234 <bsorawit1234@gmail.com>
2023-10-07 15:44:17 +0700

    not finish 2-3

--- Main.java
+++ Main.java
@@ -1,6 +1,66 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Customer {
+    String name;
+
+    public Customer(String n) {
+        name = n;
+    }
+}
+
+class Booking {
+    int bookingID;
+
+    public Booking() {
+        String path = "src/main/java/";
+        String fileName = "bookings.txt";
+        boolean openSuccess = false;
+
+        while (!openSuccess) {
+            try (Scanner scan = new Scanner(new File(path + fileName));) {
+                openSuccess = true;
+                ArrayList<Customer> CL = new ArrayList<Customer>();
+
+                for(int i = 0; scan.hasNext(); i++) {
+                    String line = scan.nextLine();
+                    String[] col = line.split(",");
+
+                    bookingID = Integer.parseInt(col[0].trim());
+
+                    Customer cm = new Customer(col[1].trim());
+
+                    boolean duplicate = false;
+                    for (Customer item : CL) {
+                        if (item.name.equals(cm.name)) {
+                            duplicate = true;
+                            // add more code
+                            break;
+                        }
+                    }
+
+                    if(!duplicate) CL.add(cm);
+
+                }
+                for(Customer c: CL) System.out.println(c.name);
+
+
+            } catch (FileNotFoundException e) {
+                System.err.println(e);
+            }
+        }
+
+
+
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD");
-        System.out.println("Branch BOOM");
+
+
+        Booking b1 = new Booking();
     }
 }
